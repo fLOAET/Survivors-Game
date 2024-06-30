9e0833fc5562b940dbc1def501c71411f3c7736c
fLOAET <logangreennz@gmail.com>
2024-07-01 11:59:27 +1200

    Added more menu buttons

--- Survivors Game/Scripts/MainMenuUI.gd
+++ Survivors Game/Scripts/MainMenuUI.gd
@@ -6,10 +6,19 @@ const WORLD = preload("res://Scenes/world.tscn")
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_btn_play_pressed():
+	print("test")
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_btn_options_pressed():
+	pass # Replace with function body.
+
+func _on_btn_credits_pressed():
+	pass # Replace with function body.
+	
+func _on_btn_quit_pressed():
+	get_tree().quit()
