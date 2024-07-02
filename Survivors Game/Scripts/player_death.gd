43f61c6128f249f4e242efb745717964b490c4df
fLOAET <logangreennz@gmail.com>
2024-07-02 12:42:20 +1200

    Small setup for death ui

--- Survivors Game/Scripts/player_death.gd
+++ Survivors Game/Scripts/player_death.gd
@@ -3,4 +3,8 @@ extends AnimatedSprite2D
 
 func _on_animation_finished():
 	get_tree().paused = true
+	await get_tree().create_timer(3).timeout
+	
+	
+	
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
