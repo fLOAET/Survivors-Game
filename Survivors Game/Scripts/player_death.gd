12ccbca40d82e36d1359c70122aa427de4977574
fLOAET <logangreennz@gmail.com>
2024-07-23 15:58:47 +1200

    sizing broken

--- Survivors Game/Scripts/player_death.gd
+++ Survivors Game/Scripts/player_death.gd
@@ -2,9 +2,6 @@ extends AnimatedSprite2D
 
 
 @onready var death_notice = $UI/Control/DeathNotice
-const Player = preload("res://Scripts/player.gd")
-
 
 func _on_animation_finished():
-	death_notice.visible = true
 	await get_tree().create_timer(3).timeout
