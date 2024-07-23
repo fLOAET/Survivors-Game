4ddaea33ac1e11177b434fffbf98ee984b4d1ca0
fLOAET <logangreennz@gmail.com>
2024-07-23 23:06:16 +1200

    death notice broken again

--- Survivors Game/Scripts/deathUI.gd
+++ Survivors Game/Scripts/deathUI.gd
@@ -1,11 +1,14 @@
 extends CanvasLayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+const WORLD = preload("res://Scenes/world.tscn")
 
+func _on_btn_restart_pressed():
+	get_tree().reload_current_scene()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _on_btn_main_menu_pressed():
+	pass
+	#get_tree().change_scene_to_file("res://Scenes/main_menu.tscn)
+
+func _on_death_notice_visibility_changed():
 	pass
