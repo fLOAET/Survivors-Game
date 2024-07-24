230d386e7215e1981b145bdad3a63a51ad11b207
fLOAET <logangreennz@gmail.com>
2024-07-25 09:32:05 +1200

    Death notice and restart is working again

--- Survivors Game/Scripts/player_death.gd
+++ Survivors Game/Scripts/player_death.gd
@@ -4,4 +4,6 @@ extends AnimatedSprite2D
 @onready var death_notice = $UI/Control/DeathNotice
 
 func _on_animation_finished():
-	await get_tree().create_timer(3).timeout
+	get_tree().paused = true
+	await get_tree().create_timer(1).timeout
+	death_notice.visible = true
--- Survivors Game/Scripts/world.gd
+++ Survivors Game/Scripts/world.gd
@@ -4,3 +4,4 @@ extends Node2D
 func _ready():
 	PlayerStats.restart()
 	WeaponShuriken.restart()
+	get_tree().paused = false
