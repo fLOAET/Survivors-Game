6b8b2c7b279150fe6ebc8c0e3696e235ee0ce401
fLOAET <logangreennz@gmail.com>
2024-07-25 15:37:16 +1200

    Change for death notice

--- Survivors Game/Scripts/player_death.gd
+++ Survivors Game/Scripts/player_death.gd
@@ -1,9 +1,9 @@
 extends AnimatedSprite2D
 
 
-@onready var death_notice = $UI/Control/DeathNotice
+@onready var death_notice = $UI/DeathNotice
 
 func _on_animation_finished():
 	get_tree().paused = true
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(0.5).timeout
 	death_notice.visible = true
