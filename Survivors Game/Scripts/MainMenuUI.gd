efa840c68927ff4ad958dd90625fe1f649f074dd
fLOAET <logangreennz@gmail.com>
2024-07-27 17:10:56 +1200

    Timer works

--- Survivors Game/Scripts/MainMenuUI.gd
+++ Survivors Game/Scripts/MainMenuUI.gd
@@ -5,7 +5,7 @@ extends CanvasLayer
 const WORLD = preload("res://Scenes/world.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ Survivors Game/Scripts/gameTimer.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+
+var time = 0
+var minutes = 0
+var seconds = 0
+@onready var timer = $Timer
+
+func _process(delta):
+	time += delta
+	seconds = fmod(time, 60)
+	minutes = fmod(time, 3600) / 60
+	timer.text = "%02d:%02d" % [minutes, seconds]
+	
