aa47593142957a2805cea0024c6a31f1aa6c7e9a
fLOAET <logangreennz@gmail.com>
2024-08-01 09:33:49 +1200

    skeleton spawns on ready

--- Survivors Game/Scripts/EnemySpawner.gd
+++ Survivors Game/Scripts/EnemySpawner.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
-@onready var spawn_points = $SpwanPoints.get_children()
+@onready var spawn_points = $SpawnPoints.get_children()
 @onready var spawn_timer = $SpawnTimer
 @onready var timer_change = $TimerChange
 @onready var world = get_node("/root/World")
 @onready var camera = $".."
 const ENEMY_SKELETON = preload("res://Scenes/enemy_skeleton.tscn")
 
+func _ready():
+	await get_tree().create_timer(0.01).timeout
+	print("spawn")
+	spawn_enemy()
+
 func spawn_enemy():
 	var enemy = ENEMY_SKELETON.instantiate()
 	var spawn_point = spawn_points.pick_random()
 	enemy.global_position = spawn_point.global_position
 	world.add_child(enemy)
+	print("timer")
 
 func _on_spawn_timer_timeout():
 	spawn_enemy()
