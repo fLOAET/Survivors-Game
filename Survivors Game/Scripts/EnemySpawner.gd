96b6c3da8316f41762da8ab9e972429ee1287b52
fLOAET <logangreennz@gmail.com>
2024-08-01 15:48:49 +1200

    skeleton spawn timings work

--- Survivors Game/Scripts/EnemySpawner.gd
+++ Survivors Game/Scripts/EnemySpawner.gd
@@ -1,29 +1,27 @@
 extends Node2D
 
-@onready var spawn_points = $SpawnPoints.get_children()
-@onready var spawn_timer = $SpawnTimer
-@onready var timer_change = $TimerChange
 @onready var world = get_node("/root/World")
 @onready var camera = $".."
+@onready var spawn_points = $SpawnPoints.get_children()
+
+@onready var skeleton_spawn_timer = $SkeletonSpawnTimer
 const ENEMY_SKELETON = preload("res://Scenes/enemy_skeleton.tscn")
+
+@onready var boss_timer = $BossTimer
 
 func _ready():
 	await get_tree().create_timer(0.01).timeout
 	print("spawn")
-	spawn_enemy()
+	spawn_skeleton()
 
-func spawn_enemy():
+func spawn_skeleton():
 	var enemy = ENEMY_SKELETON.instantiate()
 	var spawn_point = spawn_points.pick_random()
 	enemy.global_position = spawn_point.global_position
 	world.add_child(enemy)
-	print("timer")
 
-func _on_spawn_timer_timeout():
-	spawn_enemy()
-	if spawn_timer.wait_time > 1:
-		timer_change.start()
-
-func _on_timer_change_timeout():
-	spawn_timer.wait_time -= 0.1
-	print(spawn_timer.wait_time)
+func _on_skeleton_spawn_timer_timeout():
+	print(skeleton_spawn_timer.wait_time)
+	spawn_skeleton()
+	if skeleton_spawn_timer.wait_time > 0.6:
+		skeleton_spawn_timer.wait_time -= 0.1
